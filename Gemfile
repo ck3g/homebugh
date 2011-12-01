6bc848b67e6008aca4a182f47472e2d0e4af2527
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2011-12-01 20:49:07 +0200

    deploy using capistrano

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ end
 # gem 'unicorn'
 
 # Deploy with Capistrano
-# gem 'capistrano'
+gem 'capistrano'
 
 # To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
 # gem 'ruby-debug'
---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+require 'bundler/capistrano'
+
+set :application, "homebugh"
+set :repository,  "git@github.com:ck3g/homebugh.git"
+set :domain, "homebugh.info"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :user, "kalastiuz"
+
+set :deploy_to, "/home/kalastiuz/rails/#{application}"
+set :deploy_via, :export
+
+set :use_sudo, false
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :after_symlink do
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+  end
+#   task :start do ; end
+#   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
