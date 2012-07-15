45aef3a1cb90aab96e18712657df7f7f36df0f98
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2012-07-15 15:41:35 +0300

    remove trailing whitespace

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,11 @@
 class Transaction < ActiveRecord::Base
-	belongs_to :category
+  belongs_to :category
   belongs_to :user
   belongs_to :account
-  
+
   validates :summ, :presence => true, :numericality => true
   validate :cannot_be_less_than_0_01
-  
+
   def cannot_be_less_than_0_01
     errors.add(:summ, I18n.t('common.cannot_be_less_than', :value => 0.01)) if summ.to_f < 0.01
   end
