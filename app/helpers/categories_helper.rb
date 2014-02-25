4762427bfa4b64b5b1db42b6c183e3874f9b3cd1
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-02-25 21:00:07 +0200

    Refactor category form

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -8,4 +8,10 @@ module CategoriesHelper
       "bg-danger"
     end
   end
+
+  def collection_of_types
+    CategoryType.all.map do |type|
+      [CategoryType.human_attribute_name(type.name), type.id]
+    end
+  end
 end
