fb1a9bc1fd552ac5ca1b70d465055074b2b292ca
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-11-19 05:53:22 +0200

    Add service to aggregate transactions by period

---
+++ app/services/archive_transactions_service.rb
@@ -0,0 +1,37 @@
+class ArchiveTransactionsService
+  attr_reader :user, :period_started_at, :period_ended_at
+
+  def initialize(user, archive_date)
+    @user = user
+    @period_started_at = archive_date.beginning_of_month
+    @period_ended_at = archive_date.end_of_month
+  end
+
+  def archive
+    AggregatedTransaction.transaction do
+      delete_aggregations_for_current_period
+
+      user_transactions_for_period.find_each do |t|
+        at = user.aggregated_transactions.where(
+          period_started_at: period_started_at,
+          period_ended_at: period_ended_at,
+          category_id: t.category_id,
+          category_type_id: t.category_type_id
+        ).first_or_initialize
+        at.amount = at.amount.to_f + t.summ
+        at.save
+      end
+    end
+  end
+
+  private
+  def user_transactions_for_period
+    user.transactions.where(created_at: period_started_at..period_ended_at)
+  end
+  def delete_aggregations_for_current_period
+    user.aggregated_transactions.where(
+      period_started_at: period_started_at,
+      period_ended_at: period_ended_at
+    ).delete_all
+  end
+end
---
+++ spec/services/archive_transactions_service_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+describe ArchiveTransactionsService, type: :model do
+  describe '#archive' do
+    let(:user) { create :user }
+    let(:food_category) { create :spending_category, user: user }
+    let(:salary_category) { create :income_category, user: user }
+
+    before do
+      create :transaction, summ: 10.5, user: user, created_at: 1.month.ago,
+        category: food_category
+      create :transaction, summ: 30, user: user, created_at: 1.month.ago,
+        category: food_category
+      create :transaction, summ: 100, user: user, created_at: 1.month.ago,
+        category: salary_category
+
+      create :transaction, user: user, category: food_category
+      create :transaction, created_at: 1.month.ago
+    end
+
+    subject { ArchiveTransactionsService.new(user, 1.month.ago).archive }
+
+    context 'when no archived data for period' do
+      it "archives transactions for previous month" do
+        expect { subject }.to change(AggregatedTransaction, :count).by 2
+      end
+
+      it "food category has amount of 40.5" do
+        subject
+        at = user.aggregated_transactions.
+          where(category_id: food_category.id).first
+        expect(at.amount.to_f).to eq 40.5
+      end
+
+      it "salary category has amount of 100" do
+        subject
+        at = user.aggregated_transactions.
+          where(category_id: salary_category.id).first
+        expect(at.amount.to_f).to eq 100
+      end
+    end
+
+    context 'when archived data already exists' do
+      before do
+        create :aggregated_transaction, user: user, amount: 99,
+          category: salary_category,
+          category_type_id: salary_category.category_type_id
+        create :aggregated_transaction, user: user, amount: 30,
+          category: food_category,
+          category_type_id: food_category.category_type_id
+      end
+
+      it "food category has amount of 40.5" do
+        subject
+        at = user.aggregated_transactions.
+          where(category_id: food_category.id).first
+        expect(at.amount.to_f).to eq 40.5
+      end
+
+      it "salary category has amount of 100" do
+        subject
+        at = user.aggregated_transactions.
+          where(category_id: salary_category.id).first
+        expect(at.amount.to_f).to eq 100
+      end
+    end
+  end
+end
