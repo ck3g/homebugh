f33304a85c9c50880745d98021656d91deabfc6b
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-12-07 19:07:35 +0200

    Group total amount by currency

--- lib/total_amount.rb
+++ lib/total_amount.rb
@@ -1,5 +1,15 @@
 class TotalAmount
   def self.of(user)
-    user.accounts.map(&:funds).sum
+    amount = {}
+    user.accounts.each do |account|
+      key = account.decorate.unit
+      if amount.has_key? account.decorate.unit
+        amount[key] += account.funds
+      else
+        amount[key] = account.funds
+      end
+    end
+
+    amount
   end
 end
