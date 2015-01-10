202c758a92c0b4ab09780269504c71dc1a4ace46
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-01-10 20:16:29 +0200

    Decorate category type

--- app/decorators/category_decorator.rb
+++ app/decorators/category_decorator.rb
@@ -4,4 +4,14 @@ class CategoryDecorator < Draper::Decorator
   def type_icon
     object.income? ? h.up_arrow : h.down_arrow
   end
+
+  def category_type
+    if object.inactive?
+      "bg-warning"
+    elsif object.income?
+      "bg-success"
+    else
+      "bg-danger"
+    end
+  end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,14 +1,4 @@
 module CategoriesHelper
-  def category_type(category)
-    if category.inactive?
-      "bg-warning"
-    elsif category.income?
-      "bg-success"
-    else
-      "bg-danger"
-    end
-  end
-
   def collection_of_types
     CategoryType.all.map do |type|
       [CategoryType.human_attribute_name(type.name), type.id]
