a5e81e912b83d31727eb49fa2257b983ca9b5d4f
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-05-15 16:53:26 +0800

    Fix styles for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,8 @@ module ApplicationHelper
 
   def flash_class(name)
     css_classes = ["alert"]
-    css_classes << " alert-danger" if name == :alert
-    css_classes << " alert-success" if name == :notice
+    css_classes << "alert-danger" if name == 'alert'
+    css_classes << "alert-success" if name == 'notice'
     css_classes.join(" ")
   end
 
