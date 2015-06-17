4d244f5361b5687972b4492778c447bb5694db0c
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-06-17 11:05:04 +0800

    Remove duplicated configuration settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,16 +34,6 @@ Homebugh::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = false
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # Generate digests for assets URLs.
-  config.assets.digest = true
-
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
