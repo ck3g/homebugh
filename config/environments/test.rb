c1c67e2a8eea532be7b2b2644c5c20172953311e
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-01-04 18:19:10 +0100

    Use `public_file_server.headers` instead of `static_cache_control`
    Fixes:
    
    DEPRECATION WARNING: `config.static_cache_control` is deprecated and will be removed in Rails 5.1.
    Please use
    `config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }`
    instead.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,7 +18,7 @@ Homebugh::Application.configure do
 
   # Configure static asset server for tests with Cache-Control for performance.
   config.public_file_server.enabled = false
-  config.static_cache_control = "public, max-age=3600"
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
   # Raise exceptions instead of rendering exception templates
   config.action_dispatch.show_exceptions = false
