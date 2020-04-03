bdccd44bcc3a48d3ba50a96f5533d579b174df6e
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-03 08:03:52 +0200

    Add a healthy mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,4 +1,10 @@
 module Types
   class MutationType < Types::BaseObject
+    field :health, String, null: false,
+      description: "A healthy mutation"
+
+    def health
+      "A healty mutation!"
+    end
   end
 end
