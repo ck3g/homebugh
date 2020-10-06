9d1661707c2f0bc446779388740b169e05aacd43
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-10-07 00:03:48 +0200

    Update recurring payments

--- app/controllers/recurring_payments_controller.rb
+++ app/controllers/recurring_payments_controller.rb
@@ -1,7 +1,7 @@
 class RecurringPaymentsController < ApplicationController
   authorize_resource
 
-  before_action :find_recurring_payment, only: [:destroy]
+  before_action :find_recurring_payment, only: [:edit, :update, :destroy]
 
   def index
     @recurring_payments = current_user.recurring_payments
@@ -12,11 +12,22 @@ class RecurringPaymentsController < ApplicationController
   end
 
   def create
-    @recurring_payment = current_user.recurring_payments.new(create_params)
+    @recurring_payment = current_user.recurring_payments.new(safe_params)
     if @recurring_payment.save
       redirect_to recurring_payments_path, notice: t('parts.recurring_payments.successfully_created')
     else
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @recurring_payment.update(safe_params)
+      redirect_to recurring_payments_path, notice: t('parts.recurring_payments.successfully_updated')
+    else
+      render :edit
     end
   end
 
@@ -31,7 +42,7 @@ class RecurringPaymentsController < ApplicationController
     @recurring_payment ||= current_user.recurring_payments.find(params[:id])
   end
 
-  def create_params
+  def safe_params
     params.require(:recurring_payment).permit(:title, :account_id, :category_id, :amount, :frequency, :frequency_amount, :next_payment_on)
   end
 end
---
+++ spec/features/recurring_payments/edit_recurring_payment_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+feature 'Edit Recurring Payment' do
+  given!(:user) { create :user_example_com }
+  given!(:usd) { create :currency, name: 'USD', unit: '$' }
+  given!(:cash_account) { create :account, user: user, name: "Cash", currency: usd }
+  given!(:bank_account) { create :account, user: user, name: "Bank", currency: usd }
+  given!(:services) { create :spending_category, user: user, name: "Services" }
+  given!(:special_services) { create :spending_category, user: user, name: "Special Services" }
+  given!(:rp) do
+    create(:recurring_payment, title: 'Some recurring service', user: user, account: cash_account, category: services)
+  end
+
+  scenario 'a guest cannot visit edit recurring payment page' do
+    visit "/recurring_payments/#{rp.id}/edit"
+
+    expect(current_path).to eq('/users/sign_in')
+    expect(page).to have_content 'You are not authorized to access this page.'
+  end
+
+  scenario 'a logged in user can create a recurring payment' do
+    sign_in_as 'user@example.com', 'password'
+
+    visit recurring_payments_path
+    expect(current_path).to eq recurring_payments_path
+
+    within "#recurring_payment_#{rp.id}" do
+      find('a.btn-edit').click
+    end
+    expect(current_path).to eq edit_recurring_payment_path(rp)
+
+    fill_in 'Title', with: 'Updated recurring service'
+    select 'Bank [$]', from: 'recurring_payment_account_id'
+    select 'Special Services', from: 'recurring_payment_category_id'
+    fill_in 'recurring_payment_amount', with: 15
+    select_next_payment_date(10.days.from_now.to_date)
+    select 'Monthly', from: 'recurring_payment_frequency'
+    fill_in 'recurring_payment_frequency_amount', with: 1
+    click_button 'recurring_payment_submit'
+
+    expect(current_path).to eq recurring_payments_path
+    expect(page).to have_content "The recurring payment was successfully updated."
+    expect(page).to have_content "Updated recurring service"
+
+    rp.reload
+    expect(rp.title).to eq("Updated recurring service")
+    expect(rp.account).to eq(bank_account)
+    expect(rp.category).to eq(special_services)
+    expect(rp.amount).to eq(15)
+    expect(rp.frequency).to eq('monthly')
+    expect(rp.frequency_amount).to eq(1)
+    expect(rp.next_payment_on).to eq(10.days.from_now.to_date)
+  end
+end
+
+def select_next_payment_date(date)
+  select date.day, from: 'recurring_payment_next_payment_on_3i'
+  select date.strftime("%B"), from: 'recurring_payment_next_payment_on_2i'
+  select date.year, from: 'recurring_payment_next_payment_on_1i'
+end
