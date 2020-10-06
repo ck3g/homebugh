a504dd41dcfb3945356c6cc113fabf2e34d84286
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-10-06 09:48:04 +0200

    Next payment on should not be in the past

--- app/models/recurring_payment.rb
+++ app/models/recurring_payment.rb
@@ -3,12 +3,21 @@ class RecurringPayment < ApplicationRecord
   belongs_to :category
   belongs_to :account
 
-  validates :title, :user, :category, :account, presence: true
+  validates :title, :user, :category, :account, :next_payment_on, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
   validates :frequency_amount, presence: true, numericality: { greater_than_or_equal_to: 1, only_integer: true }
+  validate :ensure_next_payment_on_is_in_the_future
 
   enum frequency: { daily: 0, weekly: 1, monthly: 2, yearly: 3 }
 
   delegate :name, to: :category, prefix: true
   delegate :name, to: :account, prefix: true
+
+  private
+
+  def ensure_next_payment_on_is_in_the_future
+    if next_payment_on.present? && next_payment_on < Date.today
+      errors.add(:next_payment_on, "cannot be in the past")
+    end
+  end
 end
