b210f79282c5c542ae676a86020be69505db3228
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-10-31 21:46:15 +0100

    Migrate to secrets yml file

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Homebugh::Application.config.secret_base = '327728c33a253d7210b9321960969fa398e55954f9564901c57b44327778bf6a363ebc4dedcba3ab6fe7f926519ba5b42e8c739ababcb8a36241358d70cc93bd1'
