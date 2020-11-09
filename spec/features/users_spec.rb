39dbac7bdf5be1c18e4cb0b111c3c76e1a508c18
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-11-09 09:10:26 +0100

    Add a hidden field to detect a bot

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -7,6 +7,17 @@ feature "user registration" do
     expect(page).to have_content "A message with a confirmation link has been sent to your email address"
   end
 
+  scenario "sign up with filled in 'name' input (aka Sign up as a bot)" do
+    visit new_user_registration_path
+    fill_in 'user_name', with: 'I am a bot'
+    fill_in 'user_email', with: 'bot@example.com'
+    fill_in 'user_password', with: 'bot-password'
+    fill_in 'user_password_confirmation', with: 'bot-password'
+    click_button I18n.t(:do_sign_up)
+
+    expect(page).to have_content "We're sorry. The registration is now closed."
+  end
+
   scenario "sign in with valid credentials" do
     create :user_example_com
     sign_in_as 'user@example.com', 'password'
