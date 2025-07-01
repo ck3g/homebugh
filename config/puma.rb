4184141b95bfd7b2c595740c6938d1ff27ac2370
Vitali Tatarintev <Kalastiuz@gmail.com>
2025-07-01 11:15:42 +0200

    Update Puma configuration for multi-environment support
    This change allows Puma to run in both production and development
    environments. It sets environment-specific configurations for file
    paths, sockets, and logging, improving flexibility and ease of
    development setup.

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,28 @@
-directory '/home/deploy/apps/homebugh/current'
-environment 'production'
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
+environment rails_env
 
-# Puma process and state files
-pidfile '/home/deploy/apps/homebugh/shared/tmp/pids/puma.pid'
-state_path '/home/deploy/apps/homebugh/shared/tmp/pids/puma.state'
+if rails_env == "production"
+  directory '/home/deploy/apps/homebugh/current'
 
-# Control app socket for pumactl
-activate_control_app "unix:///home/deploy/apps/homebugh/shared/tmp/sockets/pumactl.sock"
+  # Puma process and state files
+  pidfile '/home/deploy/apps/homebugh/shared/tmp/pids/puma.pid'
+  state_path '/home/deploy/apps/homebugh/shared/tmp/pids/puma.state'
 
-# Where to bind Puma
-bind 'unix:///home/deploy/apps/homebugh/shared/tmp/sockets/puma.sock'
+  # Control app socket for pumactl
+  activate_control_app "unix:///home/deploy/apps/homebugh/shared/tmp/sockets/pumactl.sock"
 
-# Logs
-stdout_redirect '/home/deploy/apps/homebugh/shared/log/puma.stdout.log', '/home/deploy/apps/homebugh/shared/log/puma.stderr.log', true
+  # Where to bind Puma
+  bind 'unix:///home/deploy/apps/homebugh/shared/tmp/sockets/puma.sock'
+
+  # Logs
+  stdout_redirect '/home/deploy/apps/homebugh/shared/log/puma.stdout.log',
+                  '/home/deploy/apps/homebugh/shared/log/puma.stderr.log', true
+else
+  # Development defaults
+  pidfile 'tmp/pids/puma.pid'
+  state_path 'tmp/pids/puma.state'
+  bind 'tcp://127.0.0.1:3000'
+end
 
 # Threads and workers
 threads 0, 5
