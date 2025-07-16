746de5b56d56e51742d556fc44485063ba87f885
Vitali Tatarintev <Kalastiuz@gmail.com>
2025-07-16 13:35:44 +0200

    Improve budget page with empty state and responsive design

--- spec/features/budgets/view_budgets_list_spec.rb
+++ spec/features/budgets/view_budgets_list_spec.rb
@@ -8,15 +8,16 @@ feature "View budgets list" do
   end
 
   context "when user has no budgets yet" do
-    scenario "user will see empty list" do
+    scenario "user will see empty state" do
       visit root_path
       within ".dashboard-sidebar" do
         click_link "Budgets"
       end
 
       expect(current_path).to eq budgets_path
-      expect(page).to have_selector "h2", text: "Budgets"
       expect(page).to have_content "No active budgets"
+      expect(page).to have_content "Start budget planning"
+      expect(page).to have_selector ".budget-empty-state"
     end
   end
 end
