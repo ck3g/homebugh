5b40900653c50ff8ce64b27212fee3353b5afd88
Vitali Tatarintev <Kalastiuz@gmail.com>
2025-07-21 18:10:03 +0200

    Redesign welcome page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,10 +6,4 @@
 //= link icons/delete.png
 //= link icons/edit.png
 //= link icons/move_funds.png
-//= link welcome/accounts.jpg
-//= link welcome/banner-bg.jpg
-//= link welcome/budgeting.jpg
-//= link welcome/intro-bg.jpg
-//= link welcome/reports.jpg
-//= link welcome/transactions.jpg
 //= link preloader.gif
